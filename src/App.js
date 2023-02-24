1f6f4c40aad309013635806032856e51be9f2137
prince0010 <princenagac12@gmail.com>
2023-02-25 01:33:22 +0800

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
         {/* <h1 className="text-4xl font-bold"> Test </h1> */}
+        <Navbar /> 
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,50 @@
 // rafce - Generate functional component (es7 extension) 
 
 import React from 'react'
+import {FaBars, FaTimes} from 'react-icons/fa'
+import Logo from '../assets/logo.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    // px-4 is equivalent as 1rem
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+      <div>
+        {/* Logo */}
+        <img src={Logo} alt="Logo Image" style={{width: '50px'}} />
+      </div>
+      <div className='hidden'>
+        {/* menu */}
+        <ul className='flex'>
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Work</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+
+      {/* Hamburger menu*/}
+      <div className='hidden'>
+        <FaBars />
+
+      </div>
+
+      {/* Mobile Menu */}
+      <div className='hidden'>
+      <ul>
+      <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Work</li>
+          <li>Contact</li>
+      </ul>
+
+      </div>
+
+      {/* Social icons */}
+      <div className='hidden'>
+      </div>
+    </div>
   )
 }
 
